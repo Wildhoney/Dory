92bdf02de564843c6142df97bbd42aac734ae404
Wildhoney <adam.timberlake@gmail.com>
2016-02-07 01:03:34 +0000

    Added modifiedDates

--- core/publish.js
+++ core/publish.js
@@ -8,18 +8,36 @@ import yaml from 'js-yaml';
 
 glob(`${__dirname}/public/posts/*.md`, {}, async (error, files) => {
 
+    const config = yaml.safeLoad(readFileSync('dory.yml', 'utf8'));
+    const catalogue = JSON.parse(readFileSync(config.catalogue, 'utf8'));
+
     const posts = await files.reduce(async (accumulator, file) => {
 
         const stats = await new Promise(resolve => stat(file, (error, stats) => resolve(stats)));
-        const createdDate = stats.birthtime.getTime();
+        const createdDate = stats.ctime.getTime();
         const slug = parse(file).name;
+        const post = catalogue.find(item => item.slug === slug);
+
+        if (post) {
+
+            // Update the modified time if it differs from the created time, and we haven't already recorded it.
+            const modifiedDate = stats.mtime.getTime();
+            const differsToCreated = post.createdDate !== modifiedDate;
+            const notAlreadyRecorded = !!~post.modifiedDates.indexOf(modifiedDate);
+            differsToCreated && notAlreadyRecorded && post.modifiedDates.push(modifiedDate);
+
+            return post;
+
+        }
 
         accumulator.push({ slug, createdDate, modifiedDates: [] });
+
         return accumulator;
 
     }, []);
 
-    const {catalogue} = yaml.safeLoad(readFileSync('dory.yml', 'utf8'));
-    writeFile(catalogue, JSON.stringify(posts), 'utf-8');
+    console.log(posts);
+
+    //writeFile(config.catalogue, JSON.stringify(posts), 'utf-8');
 
 });
