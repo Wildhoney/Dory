d661b96676c060fa04a0cb9df568baedc98bf253
Wildhoney <adam.timberlake@gmail.com>
2016-02-07 12:49:13 +0000

    Modified logic for mtime

--- core/publish.js
+++ core/publish.js
@@ -1,43 +1,31 @@
 import 'babel-core/register';
 import 'babel-polyfill';
 
-import {stat, readFileSync, writeFile} from 'fs';
+import {stat, readFileSync, writeFile, existsSync} from 'fs';
 import {parse} from 'path';
 import glob from 'glob';
 import yaml from 'js-yaml';
+import {uniq} from 'lodash';
 
 glob(`${__dirname}/public/posts/*.md`, {}, async (error, files) => {
 
     const config = yaml.safeLoad(readFileSync('dory.yml', 'utf8'));
-    const catalogue = JSON.parse(readFileSync(config.catalogue, 'utf8'));
+    const catalogue = existsSync(config.catalogue) ? JSON.parse(readFileSync(config.catalogue, 'utf8')) : [];
 
-    const posts = await files.reduce(async (accumulator, file) => {
+    const posts = await files.reduce(async(accumulator, file) => {
 
-        const stats = await new Promise(resolve => stat(file, (error, stats) => resolve(stats)));
-        const createdDate = stats.ctime.getTime();
         const slug = parse(file).name;
+        const stats = await new Promise(resolve => stat(file, (error, stats) => resolve(stats)));
         const post = catalogue.find(item => item.slug === slug);
+        const createdDate = post.createdDate || stats.ctime.getTime();
+        const modifiedDate = stats.mtime.getTime();
+        const modifiedDates = post && post.createdDate !== modifiedDate ? uniq([ ...post.modifiedDates, modifiedDate ]) : [];
 
-        if (post) {
-
-            // Update the modified time if it differs from the created time, and we haven't already recorded it.
-            const modifiedDate = stats.mtime.getTime();
-            const differsToCreated = post.createdDate !== modifiedDate;
-            const notAlreadyRecorded = !!~post.modifiedDates.indexOf(modifiedDate);
-            differsToCreated && notAlreadyRecorded && post.modifiedDates.push(modifiedDate);
-
-            return post;
-
-        }
-
-        accumulator.push({ slug, createdDate, modifiedDates: [] });
-
+        accumulator.push({ slug, createdDate, modifiedDates });
         return accumulator;
 
     }, []);
 
-    console.log(posts);
-
-    //writeFile(config.catalogue, JSON.stringify(posts), 'utf-8');
+    writeFile(config.catalogue, JSON.stringify(posts), 'utf-8');
 
 });
