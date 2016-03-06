cd1e15b61c4192e4821eb2ae703e8a8b5186b899
Wildhoney <adam.timberlake@gmail.com>
2016-03-06 14:45:02 +0000

    Modified config endpoint

--- server/components/posts.js
+++ server/components/posts.js
@@ -12,7 +12,7 @@ export default options => {
     return (request, response) => {
 
         const pageNumber = request.params.pageNumber;
-        const perPage = options.config.posts.perPage;
+        const perPage = options.config.perPage;
         const bySlug = getPost(options);
         const posts = catalogue.slice(0, perPage).map(post => bySlug(post.slug));
 
