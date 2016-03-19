76baf70967e55c9105a5e0541d1b18ac184c3263
Wildhoney <adam.timberlake@gmail.com>
2016-03-19 14:52:14 +0000

    Added 'title' property back in catalogue file

--- core/cli/catalogue.js
+++ core/cli/catalogue.js
@@ -3,7 +3,9 @@ import 'babel-polyfill';
 import { stat, readFileSync, writeFile, existsSync, writeFileSync } from 'fs';
 import { parse } from 'path';
 import glob from 'glob';
+import { safeLoad } from 'js-yaml';
 import { uniq } from 'lodash';
+import { loadFront } from 'yaml-front-matter';
 
 const catalogue = `${__dirname}/public/catalogue.json`;
 
@@ -18,13 +20,14 @@ glob(`${__dirname}/public/posts/*`, {}, async (error, files) => {
             return new Promise(async resolve => {
 
                 const {name: slug, base: filename} = parse(file);
+                const {title} = loadFront(file, 'content');
                 const stats = await new Promise(resolve => stat(file, (error, stats) => resolve(stats)));
                 const post = blogPosts.find(item => item.filename === filename);
                 const createdDate = post && post.createdDate || stats.ctime.getTime();
                 const modifiedDate = stats.mtime.getTime();
                 const modifiedDates = post && post.createdDate !== modifiedDate ? uniq([ ...post.modifiedDates, modifiedDate ]) : [];
 
-                resolve({ slug, createdDate, modifiedDates, filename });
+                resolve({ slug, createdDate, modifiedDates, filename, title });
 
             });
 
