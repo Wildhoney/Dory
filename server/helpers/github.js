a77bca6d72c53921b8784d9e76ad6e935b7b3408
Wildhoney <adam.timberlake@gmail.com>
2016-03-23 11:51:59 +0000

    Added Redis caching

--- server/helpers/github.js
+++ server/helpers/github.js
@@ -1,8 +1,11 @@
 import GitHubApi from 'github';
+import RedisApi from 'redis';
+import { parse } from 'url';
 import { compose } from 'ramda';
-import authenticate from './authenticate';
-import { isProduction } from './common';
 import PrettyError from 'pretty-error';
+import crc32 from 'crc-32';
+import { isProduction } from './common';
+import config from '../../public/js/config';
 
 /**
  * @constant github
@@ -13,26 +16,84 @@ const github = compose(authenticate)(new GitHubApi({ version: '3.0.0', debug: !i
 }}));
 
 /**
+ * @constant redis
+ * @type {Object}
+ */
+const redis = process.env.REDIS_HOST ? RedisApi.createClient(process.env.REDIS_HOST) : mockRedis();
+
+/**
  * @method getCommits
  * @param {Object} options
  * @return {Promise}
  */
 export const getCommits = options => {
 
+    const key = Math.abs(crc32.str(JSON.stringify(options)));
+
     return new Promise((resolve, reject) => {
 
-        github.repos.getCommits(options, (error, commits) => {
+        redis.get(key, (error, response) => {
 
-            if (error) {
-                const response = JSON.parse(error.message);
-                const prettyError = new PrettyError();
-                console.log(prettyError.render(new Error(response.message)));
-                reject();
-                return;
+            if (response) {
+                return void resolve(JSON.parse(response));
             }
 
-            resolve(commits);
+            github.repos.getCommits(options, (error, response) => {
+
+                redis.set(key, JSON.stringify(response));
+                redis.expire(key, config.cacheExpiration);
+
+                if (error) {
+                    const response = JSON.parse(error.message);
+                    const prettyError = new PrettyError();
+                    console.log(prettyError.render(new Error(response.message)));
+                    reject();
+                    return;
+                }
+
+                resolve(response);
+
+            });
 
         });
+
     });
+
 };
+
+/**
+ * @method mockRedis
+ * @return {Object}
+ */
+function mockRedis() {
+
+    return {
+        set: () => {},
+        expire: () => {},
+        get: (_, callback) => callback(false, false)
+    };
+
+}
+
+/**
+ * @method authenticate
+ * @param github {Object}
+ * @return {Object}
+ */
+function authenticate(github) {
+
+    const { GITHUB_KEY, GITHUB_SECRET } = process.env;
+
+    if (GITHUB_KEY && GITHUB_SECRET) {
+
+        github.authenticate({
+            type: 'oauth',
+            key: GITHUB_KEY,
+            secret: GITHUB_SECRET
+        });
+
+    }
+
+    return github;
+
+}
