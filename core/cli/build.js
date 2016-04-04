db4a5aa959d2e3c3d1e581e556b2ced749f986f2
Wildhoney <adam.timberlake@gmail.com>
2016-04-04 10:57:20 +0100

    Added cache busting to index.html

--- core/cli/build.js
+++ core/cli/build.js
@@ -1,5 +1,6 @@
 import { readFileSync, writeFileSync } from 'fs';
 import { exec } from 'shelljs';
+import config from './package.json';
 
 // Make the necessary directory structure.
 exec('mkdir -p core/build');
@@ -7,6 +8,10 @@ exec('mkdir -p core/build');
 // Export the configuration file, and generate the SASS file.
 exec('npm run config');
 
-// Build the assets, and copy the favicon.
+// Build the assets.
 exec('webpack -p --optimize-minimize --optimize-dedupe');
 exec('html-minifier public/index.html --collapse-whitespace > core/build/index.html');
+
+// Add the cache busting parameters to the index document.
+const index = readFileSync('core/build/index.html', 'utf8');
+writeFileSync('core/build/index.html', index.replace(/\$id\$/g, config.version));
