470582f2eae744d42991727f3b04a4006f77af65
Wildhoney <adam.timberlake@gmail.com>
2016-04-04 10:10:23 +0100

    Added  to config from 'package.json' file

--- core/cli/config.js
+++ core/cli/config.js
@@ -1,5 +1,9 @@
 import { readFileSync, writeFileSync } from 'fs';
 import { load as loadYaml } from 'yaml-js';
 
-const exportConfig = `export default ${JSON.stringify(loadYaml(readFileSync('./dory.yml')))};`;
+const userConfig = loadYaml(readFileSync('./dory.yml', 'utf8'));
+const version = JSON.parse(readFileSync('./package.json', 'utf8')).version;
+const config = JSON.stringify({ ...userConfig, version });
+
+const exportConfig = `export default ${config};`;
 writeFileSync('./public/js/config.js', exportConfig, 'utf8');
